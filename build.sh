933c2ca250290461b34b833bbcc337b23ac3c284
Gj <gongjie0422@163.com>
2022-07-08 15:13:59 +0800

    chore: build.sh

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@
 VERSION=1.0.2
 
 # windows
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bot-amd64-${VERSION}.exe main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./build/bot-amd64-${VERSION}.exe main.go
 
 # MacOS 64
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o bot-darwin-amd64-${VERSION} main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./build/bot-darwin-amd64-${VERSION} main.go
 # arm
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o bot-darwin-arm64-${VERSION} main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ./build/bot-darwin-arm64-${VERSION} main.go
 
 # Linux
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bot-linux-amd64-${VERSION} main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./build/bot-linux-amd64-${VERSION} main.go
 # arm64 64位架构 ARMv8 适用于ARMv8设备
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o bot-linux-arm64-${VERSION} main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o ./build/bot-linux-arm64-${VERSION} main.go
 # arm
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o bot-linux-arm-${VERSION} main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o ./build/bot-linux-arm-${VERSION} main.go
